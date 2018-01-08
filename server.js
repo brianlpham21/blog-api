17412001ff6787ddcd3e0494a682ee6680f8ce10
Brian Pham <brianlpham21@gmail.com>
2018-01-08 11:21:33 -0800

    removed useMongClient

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ let server;
 
 function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, {UseMongoClient: true}, err => {
+    mongoose.connect(databaseUrl, err => {
       if (err) {
         return reject(err);
       }
